a3dbe1df63f78fabc44d77cc40acfdfacceadbe6
Rk619504 <rajg6.isb@gmail.com>
2025-12-24 18:46:02 -0500

    pre/post process done; WIP Package Init

--- src/lerobot_policy_bc/processor_bc.py
+++ src/lerobot_policy_bc/processor_bc.py
@@ -1,13 +1,15 @@
 # processor_bc.py
 from typing import Dict, Any
-import torch
+from lerobot.policies.processing import PolicyProcessorPipeline, PolicyAction
 
+def make_bc_pre_post_processors(config):
+    preprocessor = None # lerobot handles standard preprocessing
+    # Postprocess - select n_action_steps from predicted horizon
+    def postprocess(action: PolicyAction) -> PolicyAction:
+        # All horizon steps predicted; select first n_action_steps
+        action.values = action.values[:config.n_action_steps]
+        return action
+    
+    postprocessor = PolicyProcessorPipeline([postprocess])
 
-def make_my_custom_policy_pre_post_processors(
-    config,
-) -> tuple[
-    PolicyProcessorPipeline[dict[str, Any], dict[str, Any]],
-    PolicyProcessorPipeline[PolicyAction, PolicyAction],
-]:
-    """Create preprocessing and postprocessing functions for your policy."""
-    pass  # Define your preprocessing and postprocessing logic here
+    return preprocessor, postprocessor
